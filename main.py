357a413f912546ecc9342bec267c5eb3394401a4
bleedn <64755596+bleedn@users.noreply.github.com>
2022-08-27 12:54:26 -0400

    1.0.0
    Update to version 1.0.0, checks for sections to determine strings sections

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import struct
 
+
 # Open to see functions I used
 def UInt32(f):
     intvar = (struct.unpack("<I", f.read(4))[0])
@@ -54,49 +55,54 @@ def newline(n=1):
     for x in range(n):
         print()
 
-if not sys.argv[1]:
-    print("You didn't open this program with a file, either drag a .model onto the program to drag it as your arg in cmd")
+
+if sys.argv[1] is None:
+    print(
+        "You didn't open this program with a file, either drag a .model onto the program to drag it as your arg in cmd")
     input("Press Enter to close...")
-    exit()
+    sys.exit()
 
-try:
-    file = sys.argv[1]
-    f = open(file, 'rb')
-    f.seek(44)
-    fileSize = Int32(f)
-    sectionsamount = UInt32(f)
-
-    for i in range(sectionsamount):
-        inlist = False
-        sectiontype = UInt32(f)
-        sectionoffset = UInt32(f)
-        sectionsize = UInt32(f)
-
-    f.seek(52 + (sectionsamount * 12) + 17)
-    while True:
-        byte = f.read(2)
-        if byte == b'\x00\x00':
-            back(f, 2)
-            stringsize = f.tell() - (52+(sectionsamount * 12)+17)
-            break
+else:
+    try:
+        file = sys.argv[1]
+        f = open(file, 'rb')
+        f.seek(44)
+        fileSize = Int32(f)
+        sectionsamount = UInt32(f)
+        endstringseco = 0
+        endstringstatico = 0
+        for i in range(sectionsamount):
+            inlist = False
+            sectiontype = UInt32(f)
+            sectionoffset = UInt32(f)
+            sectionsize = UInt32(f)
+            if sectiontype == 4023987816:
+                endstringseco = sectionoffset
+            elif sectiontype == 675087235:
+                endstringstatico = sectionoffset
+        f.seek(52 + (sectionsamount * 12) + 17)
+        if not endstringseco == 0:
+            stringread = (endstringseco + 40) - (52 + (sectionsamount * 12) + 17)
         else:
-            pass
+            stringread = (endstringstatico + 40) - (52 + (sectionsamount * 12) + 17)
+        strings = f.read(stringread - 4)
+        strings = strings.decode('utf-8')
+        DataEntrys = strings.split("\x00")
+        MaterialPaths = []
+        for i in DataEntrys:
+            if ".material" in i:
+                MaterialPaths.append(DataEntrys.index(i))
 
-    f.seek(52 + (sectionsamount * 12) + 17)
-    strings = f.read(stringsize)
-    strings = strings.decode('utf-8')
-    DataEntrys = strings.split("\x00")
-    MaterialPaths = []
-    for i in DataEntrys:
-        if ".material" in i:
-            MaterialPaths.append(DataEntrys.index(i))
+        if not MaterialPaths:
+            print("Model has no material slots")
+        else:
+            for loop, i in enumerate(MaterialPaths):
+                print("Material Slot " + str(loop) + ": " + str(DataEntrys[i]))
 
-    for loop, i in enumerate(MaterialPaths):
-        print("Material Slot " + str(loop) + ": " + str(DataEntrys[i]))
+    except:
+        print('error')
+        pass
 
-except:
-    print('error')
-    pass
-
-input("Press Enter to close...")
-exit()
+    input("Press Enter to close...")
+    f.close()
+    sys.exit()
